5410ed289fbc072cd76b6d549a52151055a47560
Hank860502 <hank860502@gmail.com>
2017-09-05 15:12:49 -0400

    stick mosule done

--- MedievalTown.java
+++ MedievalTown.java
@@ -22,16 +22,32 @@ public class MedievalTown{
 		t.forward(10);
 		t.left(90);
 		t.forward(10);
+
+		// LOOP bump * 5
 		for(int i=0; i<4; i++){
+		// PART1
 			for(int j=0; j<2; j++){
 				t.right(90);
 				t.forward(10);
 			};
+			// PART2
 			for(int k=0; k<2; k++){
 				t.left(90);
 				t.forward(10);
-			}
-
+			};
 		}
+		// Split each bump on the roof to two parts, the first part handles all the right turns and forward. Second part handles all the left turns and forward. After creating one little bump loop it 5 times to create the whole roof.
+
+		t.forward(10);
+		t.left(60);
+		t.forward(x);
+		t.right(60);
+		t.forward(100);
+		t.left(90);
+		// turtle back in original start point
+	};
+
+	public static void windows(Turtle t){
+		
 	}
 }
