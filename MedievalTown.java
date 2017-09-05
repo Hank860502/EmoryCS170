2e1a2879430a9c6ee25bada3e35088455f409313
Hank860502 <hank860502@gmail.com>
2017-09-05 14:57:07 -0400

    module stick half way done, finish roof

--- MedievalTown.java
+++ MedievalTown.java
@@ -1,12 +1,37 @@
 public class MedievalTown{
 	public static void main(String[] args){
 		Turtle hank = new Turtle();
-		// square(bob, 50);
-		// triangle(bob, 40);
-		// bob.color("yellow");
-		// pentagon(bob, 40);
-		// polygon(bob, 40, 120);
-		// dashedPolygon(bob, 80, 5, 6);
-
+		
+		// hank.forward(100);
+		stick(hank);
+		// System.out.printf("" + x);
 	};
+
+	public static void stick(Turtle t){
+		double x = Math.sqrt(500);
+		// square root of 20*20 + 10*10 to get the length of the roof
+
+		t.forward(50);
+		t.left(90);
+		t.forward(100);
+		t.right(60);
+		t.forward(x);
+		t.left(60);
+		t.forward(20);
+		t.left(90);
+		t.forward(10);
+		t.left(90);
+		t.forward(10);
+		for(int i=0; i<4; i++){
+			for(int j=0; j<2; j++){
+				t.right(90);
+				t.forward(10);
+			};
+			for(int k=0; k<2; k++){
+				t.left(90);
+				t.forward(10);
+			}
+
+		}
+	}
 }
