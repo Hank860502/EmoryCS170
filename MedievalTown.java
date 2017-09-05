90db0a387ab418bf6b719efe27eda4b2377c476f
Hank860502 <hank860502@gmail.com>
2017-09-05 11:27:24 -0400

    starting hw1 medTown create turtle

---
+++ MedievalTown.java
@@ -0,0 +1,12 @@
+public class MedievalTown{
+	public static void main(String[] args){
+		Turtle hank = new Turtle();
+		// square(bob, 50);
+		// triangle(bob, 40);
+		// bob.color("yellow");
+		// pentagon(bob, 40);
+		// polygon(bob, 40, 120);
+		// dashedPolygon(bob, 80, 5, 6);
+
+	};
+}
