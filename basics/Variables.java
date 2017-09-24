985a28571e9475de6791af480eed36197f7f4493
Hank860502 <hank860502@gmail.com>
2017-09-24 14:25:46 -0400

    move hw1 to repo turtle

--- basics/Variables.java
+++ basics/Variables.java
@@ -1,8 +1,23 @@
 public class Variables{
 	public static void main(String[] arg){
-		System.out.println(multN(4));
+		// System.out.println(multN(4));
+		// System.out.println(multiplyString("hank", 4));
+		// System.out.println(multiplyString("hank", 8));
+	int a = 2;
+	double b = 2.0;
+	String c = "2";
+	// System.out.println(c*a+a);
+	System.out.println(c + a*a);
+      System.out.println(giveCandies(5, 2));
+	
 	}
 
+  public static String giveCandies(int candies, int students) {
+      return "I have " + candies + " candies. I can give "
+              + (candies / students) + " candies to each student and have "
+              + (candies % students) + " left.";
+  }
+  
 	public static int multN(int n){
 		int result = 1;
 		for(int i=1; i<=n; i++){
@@ -10,4 +25,12 @@ public class Variables{
 		}
 		return result;
 	}	
+
+	public static String multiplyString(String s, int n){
+		String x = "";
+		for(int i=0; i<n; i++){
+			x += s;
+		}
+		return x;
+	}
 }
