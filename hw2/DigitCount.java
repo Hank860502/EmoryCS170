e6611d24da18461bbe899beb909acc20bb477598
Hank860502 <hank860502@gmail.com>
2017-09-30 13:27:00 -0400

    add extreme case like negative numbers and 0 to digitcount

--- hw2/DigitCount.java
+++ hw2/DigitCount.java
@@ -7,10 +7,20 @@ public class DigitCount{
     System.out.println(digitCount(456234, 8)); // 0
     System.out.println(digitCount(456234, 5)); // 1
     System.out.println(digitCount(456234, 4)); // 2
+    System.out.println(digitCount(-456234, 4)); // 2
+    System.out.println(digitCount(0, 0)); // 1
 	}
 
 	public static int digitCount(int tested, int tester){
     int count = 0;
+    if(tested < 0){
+      tested *= -1; // make all negative numbers positive first;
+    }
+    if(tested == 0){
+      if(tester == 0){
+        count += 1;
+      }
+    } else {
       while(tested > 0) {
         int lastDigit = tested % 10; // using the modulo to get the last digit of the number
         if(tester == lastDigit){ // check if the tester matches the last digit
@@ -18,6 +28,7 @@ public class DigitCount{
         }
         tested /= 10; // divide the tested by 10 to move on to the second last digit...
       }
+    }
     return count;
   }
 }
