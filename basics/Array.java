f641e56fe76eb124c36e828b43e17d726036a5b4
Hank860502 <hank860502@gmail.com>
2017-10-14 19:13:31 -0400

    10/11 class notes

--- basics/Array.java
+++ basics/Array.java
@@ -8,11 +8,20 @@ public class Array{
 		a[2]=15;
 		a[3]=9;
 
+		double[][] h = new double[2][3];
+		h[0][0] = 1;
+		h[0][1] = 2;
+		h[0][2] = 3;
+		h[1][0] = 6;
+		h[1][1] = 5;
+		h[1][2] = 4;
+
 		System.out.println(sumAll(a));
 		System.out.println(arrayToString(a));
 		System.out.println(arrayToString(b));
 		System.out.println(minValue(a));
 		System.out.println(minIndex(a));
+		System.out.println(transpose(h, 2, 3));
 	}
 
 	public static int sumAll(int[] x){
@@ -56,4 +65,48 @@ public class Array{
 		}
 			return index;
 	}
+
+	public static int remove(int[] a, int n, int pos){
+		int result = a[pos];
+		for(int i=pos; i<n; i++){
+			a[i] = a[i+1];
+		}
+		return result;
+	}
+
+	public static	double matrixToString(double x, n, m){
+
+	}
+
+	public static double[][] transpose(double[][] x, int n, int m){
+		double[][] result = new double[m][n];
+		for(int row=0; row<n; row++){
+			for(int col=0; col<m; col++){
+				result[col][row] = x[row][col];
+			}
+		}
+		return result;
+	}
+
+
+	public static boolean isTriangular(double [][] x, int n){
+		boolean isLowerTriangular = true;
+		for(int row=0; row<n-1; row++){
+			for(int col=row+1; col<n; col++){
+				if(x[row][col] != 0){
+					isLowerTriangular = false;
+				}
+			}
+		}
+		boolean isUpperTriangular = true;
+		for(int row=1; row<n; row++){
+			for(int col=0; col<row; col++){
+				if(x[row][col] != 0){
+					isUpperTriangular = false;
+				}
+			}
+		}
+		return isUpperTriangular || isLowerTriangular;
+	}
+
 }
