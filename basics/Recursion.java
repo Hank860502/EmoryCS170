6f3055ab80e4a4dd1740143556e74dbad8c6ffd4
Hank860502 <hank860502@gmail.com>
2017-10-18 18:43:55 -0400

    10/18 lecture, further intro of recursion

--- basics/Recursion.java
+++ basics/Recursion.java
@@ -2,11 +2,22 @@ public class Recursion{
 
 	public static void main(String[] args){
 
-		System.out.println(powerOfTwo(5)); // 
-		System.out.println(factorial(3)); // 
-		System.out.println(reverseString("sdf")); //
-		System.out.println(isPalindrome("sdf")); //
-		System.out.println(isPalindrome("kayak")); //
+		// System.out.println(powerOfTwo(5)); // 
+		// System.out.println(factorial(3)); // 
+		// System.out.println(reverseString("sdf")); //
+		// System.out.println(isPalindrome("sdf")); //
+		// System.out.println(isPalindrome("kayak")); //
+
+		int[] x1 = {};
+		int[] x2 = {5};
+		int[] x3 = {2, 1, 3, 4, 10, 0};
+		// System.out.println(sum(x1, x1.length));
+		// System.out.println(sum(x2, x2.length));
+		// System.out.println(sum(x3, x3.length));
+		
+		System.out.println(min(x3, x3.length));
+
+
 
 	}
 
@@ -42,5 +53,48 @@ public class Recursion{
 			return s.charAt(0) == s.charAt(s.length()-1) && isPalindrome(s.substring(1, s.length()-1));
 		}
 	}
+
+	public static int sum(int[] x, int n){
+		if(n==0){
+			return 0;
+		} else {
+			return x[n-1] + sum(x, n-1);
+		}
+	}
+
+	public static int oddCount(int[] x, int n){
+		if(n==0){
+			return 0;
+		} else if(x[n-1] % 2 == 1){
+			return 1 + oddCount(x, n-1);
+		} else {
+			return oddCount(x, n-1);
+		}
+	}
 	
+	public static int min(int[] x, int n){
+		if(n==1){
+			return x[0];
+		} else {
+			int k = min(x, n-1);
+			if(x[n-1] < k){
+				return x[n-1];
+			} else {
+				return k;
+			}
+		}
+	}
+
+	public static int minIndex(int[] x, int n){
+		if(n==1){
+			return 0;
+		} else {
+			int k = minIndex(x, n-1);
+			if(x[n-1] < x[k]){
+				return n-1;
+			} else {
+				return k;
+			}
+		}
+	}
 }
